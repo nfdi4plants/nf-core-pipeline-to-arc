48e8cb9f857b2a171434f1553d096b0796ab74f8
Louisa Perelo <louisa.perelo@qbic.uni-tuebingen.de>
2022-05-05 12:04:36 +0200

    update main.ts and README, add minimum Test .yml

--- README.md
+++ README.md
@@ -1,12 +1,20 @@
 # nf-core-pipeline-to-arc
+
 WORK IN PROGRESS
 
-Generates an ARC ready CWL workflow and run from a nf-core pipeline's nextflow_schema.json.
+Generates an ARC ready CWL workflow and run from a nf-core pipeline's `nextflow_schema.json`.
 
 Basic commands:
-```
 
+```
 tsc main.ts --resolveJsonModule
 node main.js
 ```
 
+After creating your pipeline.cwl, you can test it by running the test version of the respective pipeline. This implies you have `nextflow` and `cwltool` installed.
+
+Command for test run:
+
+```
+cwltool pipeline.cwl minTest.yml
+```
--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
-import * as cwlTsAuto from 'cwl-ts-auto'
-import * as fs from 'fs'
-import * as http from 'https'
+import * as cwlTsAuto from "cwl-ts-auto";
+import * as fs from "fs";
+import * as http from "https";
 
 /* ************************************* */
 // GET PIPELINE INFO FROM USER BY CONSOLE INPUT //
@@ -15,126 +15,144 @@ const outname = prompt("Enter the name of your output .cwl (e.g. rnaseq): ");
 // DOWNLOAD THE NEXTFLOW_SCHEMA.JSON FROM PIPELINE'S GITHUB REPO //
 /* ************************************* */
 // path to github content
-const gitpath = "https://raw.githubusercontent.com/"
+const gitpath = "https://raw.githubusercontent.com/";
 // create path to nextflow_schema.json
-const schemapath = gitpath+pipeline+'/'+tag+'/nextflow_schema.json';
+const schemapath = gitpath + pipeline + "/" + tag + "/nextflow_schema.json";
 
 const file = fs.createWriteStream("nextflow_schema.json");
-http.get(schemapath, function(response:any) {
+http.get(schemapath, function (response: any) {
   response.pipe(file);
   // after download completed close filestream
   file.on("finish", () => {
-      file.close();
-       console.log("The nextflow_schema.json has been updated to the requested pipeline");
+    file.close();
+    console.log(
+      "The nextflow_schema.json has been updated to the requested pipeline"
+    );
   });
 });
-import * as schema from './nextflow_schema.json';
+import * as schema from "./nextflow_schema.json";
 
 /* ************************************* */
 /////////////// DEFINITIONS //////////////
 /* ************************************* */
 export interface nfInputType {
-    name: string    //id and prefix
-    type: string    //type, may depend on format, if defined
-    default: any    //default value, if given
-  }
+  name: string; //id and prefix
+  type: string; //type, may depend on format, if defined
+  default: any; //default value, if given
+}
 
 type CWLInputType =
-  | string 
-  | cwlTsAuto.CommandInputRecordSchema 
-  | cwlTsAuto.CommandInputEnumSchema 
-  | cwlTsAuto.CommandInputArraySchema 
+  | string
+  | cwlTsAuto.CommandInputRecordSchema
+  | cwlTsAuto.CommandInputEnumSchema
+  | cwlTsAuto.CommandInputArraySchema;
 
-export const formatTypes = ['file-path', 'directory-path', 'path', 'number', 'string', 'boolean']
+export const formatTypes = [
+  "file-path",
+  "directory-path",
+  "path",
+  "number",
+  "string",
+  "boolean",
+];
 /* ************************************* */
 /////////////// FUNCTIONS ////////////////
 /* ************************************* */
-function createBinding (prefix: string): cwlTsAuto.CommandLineBinding {
-    return new cwlTsAuto.CommandLineBinding({
-            prefix: prefix
-    })
+function createBinding(prefix: string): cwlTsAuto.CommandLineBinding {
+  return new cwlTsAuto.CommandLineBinding({
+    prefix: prefix,
+  });
 }
 
-export function createInput (name: string, type: CWLInputType | CWLInputType[], prefix: string, inputDefault: any): cwlTsAuto.CommandInputParameter {
-  let binding = createBinding(prefix)
+export function createInput(
+  name: string,
+  type: CWLInputType | CWLInputType[],
+  prefix: string,
+  inputDefault: any
+): cwlTsAuto.CommandInputParameter {
+  let binding = createBinding(prefix);
   //return new cwlTsAuto.CommandInputParameter({
   var newInput = new cwlTsAuto.CommandInputParameter({
-      id: name,
-      type: type,
-      inputBinding: binding,
-  })
-  newInput.default_ = inputDefault
-  return newInput
+    id: name,
+    type: type,
+    inputBinding: binding,
+  });
+  newInput.default_ = inputDefault;
+  return newInput;
 }
 
-function mapNfTypeToType (nfType: string): string {
-    // ToDo: check if nfType is in schema
-    switch (nfType) {
-      case 'file_path': {
-        return 'File'
-      }
-      case 'directory-path': {
-        return 'Directory'
-      }
-      case 'path': {
-        //ToDo: if input has ending .gz return File, otherwise return Directory
-        return 'File'
-      }
-      case 'number': {
-        //ToDo: Check if it can be int or float?
-      return 'int'
-      }
-      case 'integer': {
-        return 'int'
-        }
-      case 'string': {
-      return 'string'
-      }
-      case 'boolean': {
-        return 'boolean'
-        }
-      default: {
-        throw Error('Input type not supported: ' + nfType)
-      }
+function mapNfTypeToType(nfType: string): string {
+  // ToDo: check if nfType is in schema
+  switch (nfType) {
+    case "file_path": {
+      return "File";
+    }
+    case "directory-path": {
+      return "Directory";
+    }
+    case "path": {
+      //ToDo: if input has ending .gz return File, otherwise return Directory
+      return "File";
+    }
+    case "number": {
+      //ToDo: Check if it can be int or float?
+      return "int";
+    }
+    case "integer": {
+      return "int";
+    }
+    case "string": {
+      return "string";
+    }
+    case "boolean": {
+      return "boolean";
+    }
+    default: {
+      throw Error("Input type not supported: " + nfType);
     }
   }
+}
 
-export function getParams (schema: any, cmdltool: any) {
+export function getParams(schema: any, cmdltool: any) {
   //let i = 3;
   for (var item in schema.definitions) {
-      var category = schema.definitions[item];
-      for (var prop in category.properties) {
-          var property = category.properties;
-          // get input parameter's name and --prefix
-          var name = prop;
-          var prefix = "--"+prop
-          // get input parameter's type
-          var type = mapNfTypeToType(property[prop].type) + "?";
-          // get input parameter's format, if defined
-          if(property[prop].format) {
-              var format = property[prop].format;
-          }
-          // get input parameter's default, if defined
-          if(property[prop].default) {
-              var defaultval = property[prop].default;
-              //check if defaultval contains a relative path like ${projectDir} or ${baseDir}
-              if (defaultval.toString().includes("${projectDir}")) {
-                let re = /\${projectDir}/gi;
-                defaultval = defaultval.toString().replace(re, gitpath + pipeline +'/' + tag);
-            } else if (defaultval.toString().includes("${baseDir}")){
-                let re = /\${baseDir}/gi;
-                defaultval = defaultval.toString().replace(re, gitpath + pipeline +'/' + tag);   
-            }
-          }
-          // create Input object
-          let nfInput = createInput(name, type, prefix, defaultval)
-          // add Input to the commandline tool
-          cmdltool.inputs.push(nfInput)
-          //reset format and defaultval for next loop
-          format=null
-          defaultval=null
-          //i = i+1;      
+    var category = schema.definitions[item];
+    for (var prop in category.properties) {
+      var property = category.properties;
+      // get input parameter's name and --prefix
+      var name = prop;
+      var prefix = "--" + prop;
+      // get input parameter's type
+      var type = mapNfTypeToType(property[prop].type) + "?";
+      // get input parameter's format, if defined
+      if (property[prop].format) {
+        var format = property[prop].format;
       }
+      // get input parameter's default, if defined
+      if (property[prop].default) {
+        var defaultval = property[prop].default;
+        //check if defaultval contains a relative path like ${projectDir} or ${baseDir}
+        if (defaultval.toString().includes("${projectDir}")) {
+          let re = /\${projectDir}/gi;
+          defaultval = defaultval
+            .toString()
+            .replace(re, gitpath + pipeline + "/" + tag);
+        } else if (defaultval.toString().includes("${baseDir}")) {
+          let re = /\${baseDir}/gi;
+          defaultval = defaultval
+            .toString()
+            .replace(re, gitpath + pipeline + "/" + tag);
+        }
+      }
+      // create Input object
+      let nfInput = createInput(name, type, prefix, defaultval);
+      // add Input to the commandline tool
+      cmdltool.inputs.push(nfInput);
+      //reset format and defaultval for next loop
+      format = null;
+      defaultval = null;
+      //i = i+1;
+    }
   }
 }
 
@@ -142,39 +160,46 @@ export function getParams (schema: any, cmdltool: any) {
 /////// CREATE COMMANDLINETOOL ////////
 /* ************************************* */
 // Initialize a commandlinetool
-let nfCommandLineTool =
-new cwlTsAuto.CommandLineTool({
-    cwlVersion: cwlTsAuto.CWLVersion.V1_2,
-    class_: cwlTsAuto.CommandLineTool_class.COMMANDLINETOOL,
-    requirements: [],
-    hints: [],
-    baseCommand: [],
-    inputs: [],
-    outputs: []
-})
+let nfCommandLineTool = new cwlTsAuto.CommandLineTool({
+  cwlVersion: cwlTsAuto.CWLVersion.V1_2,
+  class_: cwlTsAuto.CommandLineTool_class.COMMANDLINETOOL,
+  requirements: [],
+  hints: [],
+  baseCommand: [],
+  inputs: [],
+  outputs: [],
+});
 // Add baseCommand
-nfCommandLineTool.baseCommand = ['nextflow', 'run', pipeline]
+nfCommandLineTool.baseCommand = ["nextflow", "run", pipeline];
 // Add optional Inputs
-getParams(schema, nfCommandLineTool)
+getParams(schema, nfCommandLineTool);
 // Add mandatory Inputs (all nf-pipelines)
-let nfInput_r = createInput("release", "float?", "-r", version)
-nfCommandLineTool.inputs.push(nfInput_r)
-let nfInput_profile = createInput("profile", "string?", "-profile", "singularity")
-nfCommandLineTool.inputs.push(nfInput_profile)
+let nfInput_r = createInput("release", "float?", "-r", Number(version));
+nfCommandLineTool.inputs.push(nfInput_r);
+let nfInput_profile = createInput(
+  "profile",
+  "string?",
+  "-profile",
+  "singularity"
+);
+nfCommandLineTool.inputs.push(nfInput_profile);
 // Add Output
 let nfOutput_out = new cwlTsAuto.CommandOutputParameter({
   type: cwlTsAuto.CWLType.DIRECTORY,
-  id: 'out_folder',
+  id: "out_folder",
   //outputBinding: new cwlTsAuto.CommandOutputBinding({ glob: '$(runtime.outdir)/$(inputs.outdir)' })
-  outputBinding: new cwlTsAuto.CommandOutputBinding({ glob: 'inputs.outdir' })
-})
-nfCommandLineTool.outputs.push(nfOutput_out)
+  outputBinding: new cwlTsAuto.CommandOutputBinding({ glob: "$(inputs.outdir)" }),
+});
+nfCommandLineTool.outputs.push(nfOutput_out);
 
 /* ************************************* */
 ////////// CREATE THE .CWL FILE //////////
 /* ************************************* */
-fs.writeFileSync(`./`+outname +`.cwl`, JSON.stringify(nfCommandLineTool.save()))
-console.log("The "+outname +".cwl file has been created successfully")
+fs.writeFileSync(
+  `./` + outname + `.cwl`,
+  JSON.stringify(nfCommandLineTool.save())
+);
+console.log("The " + outname + ".cwl file has been created successfully");
 
 /* ************************************* */
 /* CODE SNIPPETS */
@@ -263,7 +288,7 @@ export function getParameter (schema: any, cmdltool: any) {
 }*/
 //getParams(schema, nfCommandLineTool)
 // Access and print input parameters from nextflow_schema.json
- /*for (var item in schema.definitions) {
+/*for (var item in schema.definitions) {
     console.log("INPUT PARAMETERS FOR: " + item);
     var category = schema.definitions[item];
     for (var prop in category.properties) {
@@ -295,6 +320,5 @@ for (const line of config.split(/[\r\n]+/)){
   }
    */
 
-  /*let testbind = createBinding(1,"testprefix")
+/*let testbind = createBinding(1,"testprefix")
 console.log(testbind.prefix+'?', testbind.position)*/
-
